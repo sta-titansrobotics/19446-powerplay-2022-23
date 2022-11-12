6827f278607f6c5f3ffa2343d7eec4f76c8ce1fc
mrfabroa <eric.fabroa@ycdsbk12.ca>
2022-11-12 13:59:13 -0500

    create ScissorIntake class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ScissorIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ScissorIntake.java
@@ -20,6 +20,7 @@ public class ScissorIntake {
     private final double SCISSOR_WIDE_OPEN_POSITION = -1.0; // position of servo to open wide (not really useful, but anyways)
     private final double RP_TOP_POSITION = -1.0;
     private final double RP_BOTTOM_POSITION = 1.0;
+    private final double RP_HOME_POSITION = 0.0;
 
     public ScissorIntake(Servo rbtRPServo, Servo rbtScissorServo, TouchSensor rbtScissorTouch, DistanceSensor rbtConeDistanceSensor){
         this.rpServo = rbtRPServo;
@@ -36,22 +37,81 @@ public class ScissorIntake {
         return this.coneDistanceSensor.getDistance(DistanceUnit.MM) < this.CONE_DISTANCE_THRESHOLD_MM;
     }
 
+    /**
+     * Check if the scissor touch sensor is press
+     * @return true if the scissor touch sensor is pressed
+     */
     public boolean isConeInScissor(){
         return this.scissorTouch.isPressed();
     }
 
+    /**
+     * Contract scissor arms to for cone pick up
+     */
     public void contractScissor(){
         this.scissorServo.setPosition(this.SCISSOR_CONTRACT_POSITION);
     }
 
+    /**
+     * Move the scissor up to te top position on the rack and pinion
+     */
     public void moveScissorToTop(){
         this.rpServo.setPosition(this.RP_TOP_POSITION);
     }
 
+    /**
+     * Move the scissor down to the bottom position on the rack and pinion
+     */
     public void moveScissorToBottom(){
         this.rpServo.setPosition(this.RP_BOTTOM_POSITION);
         // maybe add a timeout safety stop here;
     }
+
+    /**
+     * Reset the position of the RP servo to home position
+     */
+    public void moveScissorToHome(){
+        this.rpServo.setPosition(this.RP_HOME_POSITION);
+        // maybe add a timeout safety stop here;
+    }
+
+    /**
+     * Automated action to pick up the cone
+     * @return true if the action completes
+     */
+    public boolean autoPickUpCone(){
+        // move to home position
+        if (this.rpServo.getPosition() != this.RP_HOME_POSITION){
+            this.rpServo.setPosition(this.RP_HOME_POSITION);
+        }
+
+        // move the scissor down until bottom or cone detected in scissor
+        if (isConeInBracket()){
+            while (this.rpServo.getPosition() > this.RP_BOTTOM_POSITION || !this.isConeInScissor()){
+                this.rpServo.setPosition(this.rpServo.getPosition() + 0.1);
+            }
+
+            if (this.isConeInScissor()){
+                this.contractScissor();
+                this.moveScissorToTop();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    public void releaseCone(){
+        this.scissorServo.setPosition(0);
+    }
+
+
+
+
+
+
+
 
 
 
