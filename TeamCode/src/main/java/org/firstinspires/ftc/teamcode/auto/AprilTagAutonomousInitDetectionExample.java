e19ab63cc6cf7331ed833036bd638958d4859010
Andrew <andrew.chan23@ycdsbk12.ca>
2022-11-16 16:07:57 -0500

    Disable opmodes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AprilTagAutonomousInitDetectionExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AprilTagAutonomousInitDetectionExample.java
@@ -21,6 +21,7 @@
 
 package org.firstinspires.ftc.teamcode.auto;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -33,7 +34,7 @@ import org.openftc.easyopencv.OpenCvInternalCamera;
 
 import java.util.ArrayList;
 
-@TeleOp
+@Disabled
 public class AprilTagAutonomousInitDetectionExample extends LinearOpMode
 {
     OpenCvCamera camera;
