354bf4bae97ebab6b1d5591c6feae33fd6d2e908
shan <shan2005an@gmail.com>
2022-11-17 10:41:39 -0500

    test class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ColorSensorTest.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.util.Range;
+
+
+@TeleOp
+public class ColorSensorTest extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() {
+
+
+
+        ColorSensor cSensor = hardwareMap.get(ColorSensor.class, "distanceSensor");
+
+
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+
+
+
+
+
+            telemetry.addData("Amount of Red:", cSensor.red());
+
+            telemetry.update();
+
+        }
+
+    }
+
+
+
+
+}
